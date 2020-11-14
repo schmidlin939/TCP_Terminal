5ae1f23603017c77e1df6cd092b2b3dbf2ca543f
schmidlin939 <schmidlin939@gmail.com>
2020-11-14 17:50:22 +0100

    Add project files.

---
+++ TCP_Terminal/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TCP_Terminal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool server = false;
+        bool client = false;
+        string IP_Adress;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+         private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void cbkServer_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cbkClient_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ TCP_Terminal/TCP_Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP_Terminal
+{
+    class TCP_Client
+    {
+
+        public void Connect(string IP_Adress)
+        {
+        }
+    }
+}
